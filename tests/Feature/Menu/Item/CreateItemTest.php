e55bce3b23769b48d7e73f8e12aec326a9e84fd9
Vinicius Vicente <vinyvicente@gmail.com>
2020-03-29 18:38:00 -0300

    Added test for validation items

--- tests/Feature/Menu/Item/CreateItemTest.php
+++ tests/Feature/Menu/Item/CreateItemTest.php
@@ -15,7 +15,27 @@ class CreateItemTest extends TestCase
             ['title' => 'Item 1',],
             ['title' => 'Item 2',],
         ]);
-        
+
         $response->assertStatus(201);
+    }
+
+    public function testCreateItemsFromMenuWithItemWrong()
+    {
+        factory(Menu::class)->create();
+
+        $response = $this->postJson('/api/menus/1/items', [
+            ['title2' => 'Item 1',],
+            ['title' => 'Item 2',],
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    '0.title' => [
+                        'The 0.title field is required.'
+                    ]
+                ]
+            ]);
     }
 }
